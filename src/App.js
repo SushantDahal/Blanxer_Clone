d59df1f2233447372f377558b9299e6e243d2bac
SushantDahal <SushanDahal246@gmail.com>
2023-09-17 22:19:53 +0545

    Adding laptop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
+import Laptop from "./Components/Laptop";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Laptop />
     </div>
   );
 }
---
+++ src/Components/Laptop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Lapi from "../Assets/laptop.webp";
+
+function Laptop() {
+  return (
+    <div className="bg-red max-w-[1240px] mx-auto">
+      <div>
+        <img src={Lapi} alt="/" />
+      </div>
+    </div>
+  );
+}
+
+export default Laptop;
