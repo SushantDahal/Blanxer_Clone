41c8833a4a4c4ee18bb9bdff18f03d2916cb3e0a
SushantDahal <SushanDahal246@gmail.com>
2023-09-20 23:09:05 +0545

    Adding premium cards

--- src/Components/Premium.jsx
+++ src/Components/Premium.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { AiOutlineCheck } from "react-icons/ai";
 function Premium() {
   return (
-    <div className="bg-red-500 pb-10 py-4 max-w-[1240px] mx-auto">
+    <div className=" pb-10 py-4 max-w-[1240px] mx-auto">
       <h1 className="text-center md:text-4xl text-[33px] px-4   font-bold">
         Premium plan for your business <br />
         starting as low as <span className="text-[#913FFF]">NPR 29/day</span>.
@@ -13,9 +13,9 @@ function Premium() {
       </p>
       <div
         id="premium"
-        className="flex justify-center gap-4 items-center py-2 pb-6 bg-gray-600 mx-2 col-span-3 "
+        className="flex justify-center gap-4 items-center py-2 pb-6  mx-2 col-span-3 "
       >
-        <div className="bg-white ">
+        <div className=" ">
           <h1 className="md:text-2xl text-xl font-bold pb-1">Quaterly</h1>
           <p className="text-[12px] md:text-xl">(3 months)</p>
         </div>
@@ -23,7 +23,7 @@ function Premium() {
           <div className="bg-white w-6 h-6 rounded-[100%]"></div>
         </div>
         <div className="md:flex grid  items-center">
-          <div className="bg-white md:mr-2 m-0  ">
+          <div className=" md:mr-2 m-0  ">
             <h1 className="md:text-2xl text-xl font-bold pb-1"> Yearly</h1>
             <p className="text-[12px] md:text-xl">(12 months)</p>
           </div>
@@ -34,7 +34,170 @@ function Premium() {
           </div>
         </div>
       </div>
-      <div className="grid md:grid-cols-3 gap-2 mx-4 p-2"></div>
+      <div className="grid md:grid-cols-3 gap-2 mx-4 p-2 my-5">
+        <div className="bg-white rounded-[15px] mt-10  ">
+          <div className="text-center">
+            <h2 className="text-xl py-3 font-bold">Free</h2>
+            <p className="text-[20px] pb-4  font-serif">For Learners</p>
+
+            <h1 className="text-2xl md:text-3xl font-bold pb-8 ">NPR 0 /mo</h1>
+            <button className=" mx-auto py-3 w-[140px] rounded-full mt-6 text-white bg-[#913FFF] ">
+              Start for free
+            </button>
+          </div>
+          <div>
+            <ul className="mx-4 font-sans  text-[14px]">
+              <li className="flex items-center pt-6 pb-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />
+                Upto 15 products
+              </li>
+              <li className="flex items-center py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />
+                Basic Analytics
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Plugins
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Order
+                Management
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Inventory Management
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Basic
+                Website Customization
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Default Blanxer Website Theme
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="bg-white rounded-[15px]  border-[3px] border-[#913FFF] mt-10 md:m-0 ">
+          <div className="text-center">
+            <div className="h-10 bg-[#913FFF]  rounded-t-[10px]">
+              <h2 className="text-center pt-2">Most Popular🔥</h2>
+            </div>
+            <h2 className="text-xl py-3 font-bold">Premium</h2>
+            <p className="text-[20px] pb-4  font-serif">
+              For Small Businesses and Influencers
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold  ">NPR 1167 /mo</h1>
+            <p className="mb-4 py-2 font-sans">Rs 3499 Quarterly Billing</p>
+            <button className=" mx-auto py-3 w-[140px] rounded-full  text-white bg-[#913FFF] ">
+              Get Started
+            </button>
+          </div>
+          <div>
+            <ul className="mx-4 font-sans  text-[14px]">
+              <li className="flex items-center pt-6 pb-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />
+                Unlimited products
+              </li>
+              <li className="flex items-center py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Straff
+                Store
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                custome Domain Integration
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Order
+                SMS Plugin
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Online Payment Integration
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Basic
+                Blanxer Pay
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Website SEO
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Customers Data Management
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Order
+                Bulk Upload & Media Manager
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic All
+                available Plugin Integrations{" "}
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Basic
+                Custom Favicon Icon
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Customer Support
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Discounts Codes (Releasing soon)
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Advanced Analytics (Releasing soon)
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Custom Checkout Fields (Releasing soon)
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic
+                Product Reviews and Ratings (Releasing soon)
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="bg-white rounded-[15px] mt-10  border  ">
+          <div className="text-center">
+            <h2 className="text-xl py-3 font-bold">Platinum</h2>
+            <p className="text-[20px] pb-4  font-serif">
+              Big D2C Brands, and growing businesses
+            </p>
+
+            <h1 className="text-2xl md:text-3xl font-bold ">NPR 3267 /mo</h1>
+            <p className="mb-3 py-2 font-sans">Rs 9800 Quarterly Billing</p>
+            <button className=" mx-auto py-3 w-[140px] rounded-full text-white bg-[#913FFF] ">
+              Get Started
+            </button>
+          </div>
+          <div>
+            <ul className="mx-4 font-sans text-[14px]">
+              <li className="flex items-center pt-6 pb-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />
+                Everything in Premium
+              </li>
+              <li className="flex items-center py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />
+                10 Staff Store account
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic 1
+                Dedicated Expert for Store Management
+              </li>
+              <li className="flex items-center  py-2">
+                <AiOutlineCheck className="mr-2 text-[#913FFF] " />5 Basic Order
+                99% website uptime guaranteed Customer Support
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
